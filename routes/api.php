6a69e6200d751b9ad1c0fa1353388d6549dc1296
bushrakhalid786 <bushrakhalid786@gmail.com>
2019-02-03 16:09:08 +0100

    [fix] API to Api. add public folder to gitignore

--- routes/api.php
+++ routes/api.php
@@ -21,7 +21,7 @@ Route::middleware('jwt.auth')->get('users', function () {
     return auth('api')->user();
 });
 
-Route::post('/register', 'Api\APILoginController@register');
+Route::post('/register', 'Api\ApiLoginController@register');
 
-Route::post('/login', 'Api\APILoginController@login');
-Route::post('/logout', 'Api\APILoginController@logout');
+Route::post('/login', 'Api\ApiLoginController@login');
+Route::post('/logout', 'Api\ApiLoginController@logout');
